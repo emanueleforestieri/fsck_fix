624ff6234db3e9f9388dab1feab12907611965ac
Emanuele Forestieri <forestieriemanuele@gmail.com>
2015-10-11 15:11:05 +0200

    Update fsck_fix.sh

--- fsck_fix.sh
+++ fsck_fix.sh
@@ -23,54 +23,54 @@ export NORMAL='\033[1;00m'
 
 err_echo () #echo on stderr
 {
-	>&2 echo -e "$*"
+    >&2 echo -e "$*"
 } 
 
 err ()
 {
-	err_echo $RED"[!] "$*$NORMAL
+    err_echo $RED"[!] "$*$NORMAL
 }
 
 inf ()
 {
-	err_echo $BLUE"[*] "$*$NORMAL
+    err_echo $BLUE"[*] "$*$NORMAL
 }
 
 ok ()
 {
-	err_echo $BLUE"[+] "$*$NORMAL
+    err_echo $BLUE"[+] "$*$NORMAL
 }
 
 help()
 {
-	err_echo "Use: "$0" [partition]\n" 
-	err_echo "Options:"
-	err_echo "-h, --help          Show this screen and exit\n"
-	exit 1
+    err_echo "Use: "$0" [partition]\n" 
+    err_echo "Options:"
+    err_echo "-h, --help          Show this screen and exit\n"
+    exit 1
 }
 
 if [ "$#" -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 then 
-	help
+    help
 else	
-	if [ `id -u` -eq 0 ] #if it is run as root
-	then 
-		if [ "$1" = "`fdisk -l 2>/dev/null | grep "$1" | cut -b 1-${#1}`" ] #verify that the partition is valid
-		then
-			ok "Partition exist"
-			inf "Unmounting partition..."
-			umount $1 2>/dev/null
-			ok "Partition unmounted"
-			inf "Fixing partition with fsck..."
-			yes | fsck -CV $1 >/dev/null
-			ok "Partition fixed"
-		else
-			err "Partition does not exist"
-			exit 1
-		fi
+    if [ `id -u` -eq 0 ] #if it is run as root
+    then 
+	if [ "$1" = "`fdisk -l 2>/dev/null | grep "$1" | cut -b 1-${#1}`" ] #verify that the partition is valid
+	then
+     	    ok "Partition exist"
+	    inf "Unmounting partition..."
+	    umount $1 2>/dev/null
+	    ok "Partition unmounted"
+	    inf "Fixing partition with fsck..."
+	    yes | fsck -CV $1 >/dev/null
+	    ok "Partition fixed"
 	else
-		err "This script must be run as root"
-		exit 1
+	    err "Partition does not exist"
+	    exit 1
 	fi
+    else
+        err "This script must be run as root"
+	exit 1
+    fi
 fi
 exit 0
